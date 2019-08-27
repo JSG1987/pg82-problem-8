d00f7d4b404c148c56db4053f54bfaff0ee805c2
JSG1987 <JGentry0058@gmail.com>
2019-08-27 14:53:44 -0500

    HW8

---
+++ pg82 problem 8/Source.cpp
@@ -0,0 +1,28 @@
+// The goal of this program is to total purchace 
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	float item1 = 15.95,
+		item2 = 24.95,
+		item3 = 6.95,
+		item4 = 12.95,
+		item5 = 3.95;
+	float total = item1 + item2 + item3 + item4 + item5;
+	cout << "item 1 =" << item1 << endl;
+	cout << "item 2 =" << item2 << endl;
+	cout << "item 3 =" << item3 << endl;
+	cout << "item 4 =" << item4 << endl;
+	cout << "item 5 =" << item5 << endl;
+	cout << "----------------\n";
+	cout << "total = " << total<<endl;
+
+
+
+
+	system("PAUSE");
+	return 0;
+}
